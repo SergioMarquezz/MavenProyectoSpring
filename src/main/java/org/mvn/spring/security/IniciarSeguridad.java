4915bb9d5ab2f64c55a1ee484ba3ccb0ec18640b
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-02-10 21:46:05 -0600

    Se configura spring security con login por default

---
+++ src/main/java/org/mvn/spring/security/IniciarSeguridad.java
@@ -0,0 +1,9 @@
+package org.mvn.spring.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class IniciarSeguridad extends AbstractSecurityWebApplicationInitializer {
+	
+	
+
+}
